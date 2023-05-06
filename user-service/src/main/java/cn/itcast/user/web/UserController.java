c5954b93a93bc4dd7ba5e1ee51646d23e2fdce28
sha-mo-xuan-shen <2052039701@qq.com>
2023-05-06 17:10:14 +0800

    用了nginx搞的集群

--- user-service/src/main/java/cn/itcast/user/web/UserController.java
+++ user-service/src/main/java/cn/itcast/user/web/UserController.java
@@ -1,11 +1,10 @@
 package cn.itcast.user.web;
 
+import cn.itcast.user.config.PatternProperties;
 import cn.itcast.user.pojo.User;
 import cn.itcast.user.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,18 +16,25 @@ import java.time.format.DateTimeFormatter;
 @Slf4j
 @RestController
 @RequestMapping("/user")
-@RefreshScope
+// @RefreshScope
 public class UserController {
 
     @Autowired
     private UserService userService;
+    //
+    // @Value("${pattern.dateformat}")
+    // private String dateformat;
 
-    @Value("${pattern.dateformate}")
-    private String dateformate;
+    @Autowired
+    private PatternProperties properties;
 
+    @GetMapping("prop")
+    public PatternProperties properties(){
+        return properties;
+    }
     @GetMapping("now")
     public String now(){
-        return LocalDateTime.now().format(DateTimeFormatter.ofPattern(dateformate));
+        return LocalDateTime.now().format(DateTimeFormatter.ofPattern(properties.getDateformat()));
     }
     /**
      * 路径： /user/110
